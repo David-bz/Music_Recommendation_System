6a104b918a799b3e114d266add4e97962a329263
orshemesh <orshemesh@campus.technion.ac.il>
2020-04-23 09:17:31 +0300

    Took up to 2000 loved songs + up to 200 recent played song for each users
    Adding these datasets:
    	Songs: [id, name, artist], 611K rows
    	Recent played: [user_id, song_id, timestamp], 998K rows
    	Love: [user_id, song_id, timestamp], 774K rows
    
    TODO: convert recent played to timestamp
    >>> recent.tail()
            user_id  song_id           timestamp
    998888    14954   611782  07 Jan 2020, 14:34
    998889    14954   611870  07 Jan 2020, 14:27
    998890    14954   611871  07 Jan 2020, 14:24
    998891    14954   147325  07 Jan 2020, 14:21
    998892    14954   367502  06 Jan 2020, 17:32

--- dataset/data_mining.py
+++ dataset/data_mining.py
@@ -97,8 +97,11 @@ def israelis_users(usernames, metadata_mapping, net):
     pd.DataFrame.to_csv(users, './entities/users.csv.zip', index=False, header=True, compression='zip')
 
 class Songs():
-    def __init__(self):
-        self.songs = pd.DataFrame(columns=['id', 'name', 'artist'])
+    def __init__(self, path=''):
+        if path == '':
+            self.songs = pd.DataFrame(columns=['id', 'name', 'artist'])
+        else:
+            self.songs = pd.read_csv(path , header=0, compression='zip')
         self.lock = threading.Lock()
 
     def get_song_id(self,song_name,artist):
@@ -106,11 +109,12 @@ class Songs():
         self.lock.acquire()
         try:
             try:
-                song_id = self.songs[(self.songs.name == song_name) & (self.songs.artist == artist)].id.values[0]
+                song_id = [id for (id, s, a) in zip(self.songs.id.values, self.songs.name.values, self.songs.artist.values) if s == song_name and a == artist][0]
+                # song_id = self.songs[(self.songs.name == song_name) & (self.songs.artist == artist)].id.values[0]
             except:
                 song_id = len(self.songs)
                 self.songs = self.songs.append({'id': song_id, 'name': song_name, 'artist': artist}, ignore_index=True)
-                if len(self.songs) % 10000 == 0:
+                if len(self.songs) % 5000 == 0:
                     self.dump_data(True)
         except Exception as e:
             logging.error('Exception get_song_id({},{}): {}'.format(song_name,artist,e))
@@ -130,8 +134,11 @@ class Songs():
             pd.DataFrame.to_csv(self.songs, './entities/songs.csv.zip', index=False, header=True, compression='zip')
 
 class Love():
-    def __init__(self):
-        self.love = pd.DataFrame(columns=['user_id', 'song_id', 'timestamp'])
+    def __init__(self, path=''):
+        if path == '':
+            self.love = pd.DataFrame(columns=['user_id', 'song_id', 'timestamp'])
+        else:
+            self.love = pd.read_csv(path , header=0, compression='zip')
         self.lock = threading.Lock()
 
     def addList(self, user_id, songs : Songs, songs_list):
@@ -140,7 +147,7 @@ class Love():
             for played_song in songs_list:
                 song = played_song[0]
                 song_name = song.get_name()
-                song_artist = song.get_artist()
+                song_artist = song.get_artist().get_name()
                 timestamp = played_song[2]
                 self._add(user_id, songs.get_song_id(song_name,song_artist), timestamp)
         except Exception as e:
@@ -150,7 +157,7 @@ class Love():
     def _add(self, user_id, song_id, timestamp):
         try:
             self.love = self.love.append({'user_id': user_id, 'song_id': song_id, 'timestamp': timestamp}, ignore_index=True)
-            if len(self.love) % 10000 == 0:
+            if len(self.love) % 5000 == 0:
                 self.dump_data(True)
         except Exception as e:
             logging.error('Exception add({},{},{}): {}'.format(user_id, song_id, timestamp, e))
@@ -161,7 +168,42 @@ class Love():
         else:
             pd.DataFrame.to_csv(self.love, './relations/love.csv.zip', index=False, header=True, compression='zip')
 
-def get_users_songs_thread(net : pylast.LastFMNetwork, users, songs: Songs, love: Love, ids, thread_num):
+class RecentPlayed():
+    def __init__(self, path=''):
+        if path == '':
+            self.recent = pd.DataFrame(columns=['user_id', 'song_id', 'timestamp'])
+        else:
+            self.recent = pd.read_csv(path , header=0, compression='zip')
+        self.lock = threading.Lock()
+
+    def addList(self, user_id, songs : Songs, songs_list):
+        self.lock.acquire()
+        try:
+            for played_song in songs_list:
+                song = played_song[0]
+                song_name = song.get_name()
+                song_artist = song.get_artist().get_name()
+                timestamp = played_song[2]
+                self._add(user_id, songs.get_song_id(song_name,song_artist), timestamp)
+        except Exception as e:
+            logging.error('Exception addList({},_): {}'.format(user_id,e))
+        self.lock.release()
+
+    def _add(self, user_id, song_id, timestamp):
+        try:
+            self.recent = self.recent.append({'user_id': user_id, 'song_id': song_id, 'timestamp': timestamp}, ignore_index=True)
+            if len(self.recent) % 5000 == 0:
+                self.dump_data(True)
+        except Exception as e:
+            logging.error('Exception add({},{},{}): {}'.format(user_id, song_id, timestamp, e))
+
+    def dump_data(self, temp=False):
+        if temp:
+            pd.DataFrame.to_csv(self.recent, './relations/tmp_recent_played.csv.zip', index=False, header=True, compression='zip')
+        else:
+            pd.DataFrame.to_csv(self.recent, './relations/recent_played.csv.zip', index=False, header=True, compression='zip')
+
+def get_users_loved_songs_thread(net : pylast.LastFMNetwork, users, songs: Songs, love: Love, ids, thread_num):
     id = -1
     try:
         for i,id in enumerate(ids):
@@ -173,13 +215,13 @@ def get_users_songs_thread(net : pylast.LastFMNetwork, users, songs: Songs, love
                     break
                 user = net.get_user(username)
                 songs_list = None
-                for try_i in [1,2,3,4,5]:
+                for try_i in [1,2,3,4,5,6,7,8,9,10]:
                     try:
                         songs_list = user.get_loved_tracks(limit=1000, page=page)
                         break
                     except Exception as e:
                         logging.info('thread {} try {} id {} page {}: {}'.format(thread_num, try_i, id, page, e))
-                        time.sleep(4)
+                        time.sleep(5)
                         continue
                 if songs_list is None:
                     logging.error('Failed to get loved songs: thread {}, id {}, page {}'.format(thread_num, id, page))
@@ -190,17 +232,42 @@ def get_users_songs_thread(net : pylast.LastFMNetwork, users, songs: Songs, love
     except Exception as e:
         logging.error('Exception get_users_songs_thread id {}: {}'.format(id,e))
 
+def get_users_recent_songs_thread(net : pylast.LastFMNetwork, users, songs: Songs, recent: RecentPlayed, ids, thread_num):
+    id = -1
+    try:
+        for i,id in enumerate(ids):
+            username = users[users.id == id].lastfm_username.values[0]
+            user = net.get_user(username)
 
-def get_users_songs(threads_num):
+            songs_list = None
+            for try_i in [1,2,3,4,5,6,7,8,9,10]:
+                try:
+                    songs_list = user.get_recent_tracks(limit=200, page=1)
+                    break
+                except Exception as e:
+                    logging.info('thread {} try {} id {}: {}'.format(thread_num, try_i, id, e))
+                    time.sleep(4)
+            if songs_list is None:
+                logging.error('Failed to get recent songs: thread {}, id {}'.format(thread_num, id))
+                continue
+            recent.addList(id, songs, songs_list)
+            if i % 10 == 0:
+                logging.info('thread {} id {} out of {}'.format(thread_num, i, len(ids)))
+    except Exception as e:
+        logging.error('Exception get_users_songs_thread id {}: {}'.format(id,e))
+
+def get_users_loved_songs(threads_num):
     users = pd.read_csv('./entities/israelis_users.csv.zip', header=0, compression='zip')
-    songs = Songs()
-    love = Love()
+    songs = Songs('./entities/tmp_songs.csv.zip')
+    love = Love('./relations/tmp_love.csv.zip')
     net = init_lastfm()
     start = time.time()
 
-    user_ids_list = np.array_split(users.id.values, threads_num)
+    ids = [id for id in users.id.values if id not in love.love.user_id.values]
+
+    user_ids_list = np.array_split(ids, threads_num)
     for thread_i in range(threads_num):
-        thread = threading.Thread(target=get_users_songs_thread, args=(net, users, songs, love, user_ids_list[thread_i], thread_i))
+        thread = threading.Thread(target=get_users_loved_songs_thread, args=(net, users, songs, love, user_ids_list[thread_i], thread_i))
         thread.start()
 
     while (threading.active_count() > 1):
@@ -209,6 +276,27 @@ def get_users_songs(threads_num):
         time.sleep(10)
     songs.dump_data()
     love.dump_data()
+
+def get_users_recent_songs(threads_num):
+    users = pd.read_csv('./entities/israelis_users.csv.zip', header=0, compression='zip')
+    songs = Songs('./entities/tmp_songs.csv.zip')
+    recent = RecentPlayed('./relations/tmp_recent_played.csv.zip')
+    net = init_lastfm()
+    start = time.time()
+
+    ids = [id for id in users.id.values if id not in recent.recent.user_id.values]
+
+    user_ids_list = np.array_split(ids, threads_num)
+    for thread_i in range(threads_num):
+        thread = threading.Thread(target=get_users_recent_songs_thread, args=(net, users, songs, recent, user_ids_list[thread_i], thread_i))
+        thread.start()
+
+    while (threading.active_count() > 1):
+        current = time.time()
+        logging.info('threads {} songs {} recent {} time {}'.format(threading.active_count(), len(songs.songs), len(recent.recent), current-start))
+        time.sleep(10)
+    songs.dump_data()
+    recent.dump_data()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -227,7 +315,28 @@ logging.basicConfig(
     ]
 )
 
-get_users_songs(5)
+
+get_users_recent_songs(5)
+
+# def foo(sl):
+#     start = time.time()
+#     for ps in sl:
+#         s = ps[0]
+#         song_name = s.get_name()
+#         song_artist = s.get_artist()
+#         song_id = [id for i,s,a in zip(songs.id.values, songs.name.values, songs.artist.values) if s == song_name and a == song_artist]
+#         # s_names = songs.name.values
+#         # a_names = songs.artist.values
+#         # song_id = songs[(songs.name == song_name) & (songs.artist == song_artist)].id.values
+#         song_id = songs.query('(name == @song_name) and (artist == @song_artist)')
+#         if len(song_id) > 0:
+#             print(song_id)
+#         else:
+#             print('N')
+#     print(time.time() - start)
+
+
+
 #
 # # class Songsfff():
 # #     def __init__(self):
