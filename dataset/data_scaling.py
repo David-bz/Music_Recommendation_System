9130c4f2ddaa16054742757cad4cb10f00dc41e2
David-bz <davidbzach@gmail.com>
2020-08-27 14:55:56 +0300

    updated verified israeli users

---
+++ dataset/data_scaling.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+
+def output_feature(feature):
+
+
+if __name__ == '__main__':
+    israeli_users = pd.read_csv('./entities/verified_israeli_users.csv.zip', index_col=0, compression='zip')
+
+
+
+
+
+
+
