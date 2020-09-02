20399e83848b47e1d77e61de0cd44da064289dbe
David-bz <davidbzach@gmail.com>
2020-09-02 19:36:33 +0300

    [Data_scaling] Few features remain to scale

--- dataset/data_scaling.py
+++ dataset/data_scaling.py
@@ -1,33 +1,76 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler, PowerTransformer
-
+from sklearn.preprocessing import StandardScaler, PowerTransformer, KBinsDiscretizer
+from time import gmtime
 
 class Dataset:
-    def __init__(self):
+    def __init__(self, verbose = False):
         self.tracks =  pd.read_csv('./entities/tracks.csv.zip', compression='zip')
         self.users = pd.read_csv('./entities/users.csv.zip', index_col=0, compression='zip')
         self.scalers = {'users' : {}, 'tracks' : {}}
+        self.verbose = verbose
 
-    def scale_users(self):
+    def plot_and_save(self, data, feature_name, bins = 50, period='before'):
+        hist, bins, _ = plt.hist(data, bins=bins)
+        plt.xlabel(feature_name)
+        plt.savefig('./distributions/users/' + feature_name + '/' + period + '_scale.png')
+        plt.show()
+
+    def scale_users(self, verbose = False):
+        if verbose:
+            self.verbose = True
         self.scale_users_playcount()
-
+        self.scale_users_loved_tracks()
+        self.scale_users_artists_count()
+        self.scale_registration_year()
+        self.scale_last_activity_year()
+        self.scale_activity_period_days()
 
     def scale_users_playcount(self, verbose=False):
         # Avoid the case when users.playcount < users.loved_tracks (match to the loved_tracks)
         self.users['playcount'] = self.users.apply(lambda row : row['loved_tracks'] if row['playcount'] < row['loved_tracks'] else row['playcount'], axis=1)
-        power_scaler = PowerTransformer(method='box-cox')
         data = self.users['playcount'].values.reshape(-1, 1)
-        power_scaler = power_scaler.fit(data)
-        pc = power_scaler.transform(data)
-        if verbose:
-            hist, bins, _ = plt.hist(pc, bins=50)
-            plt.savefig('./distributions/users/playcount/after_scale.png')
-            plt.show()
+        if self.verbose: self.plot_and_save(data, 'playcount')
+        power_scaler = PowerTransformer(method='box-cox').fit(data)
+        playcount = power_scaler.transform(data)
+        if self.verbose: self.plot_and_save(playcount, 'playcount', period='after')
+
         self.scalers['users']['playcount'] = power_scaler
 
+    def scale_users_loved_tracks(self):
+        data = self.users['loved_tracks'].values.reshape(-1, 1)
+        if self.verbose: self.plot_and_save(data, 'loved_tracks')
+        power_scaler = PowerTransformer(method='yeo-johnson').fit(data)
+        loved_tracks = power_scaler.transform(data)
+        if self.verbose: self.plot_and_save(loved_tracks, 'loved_tracks', period='after')
 
+    def scale_users_artists_count(self):
+        data = self.users['artists_count'].values.reshape(-1, 1)
+        if self.verbose: self.plot_and_save(data, 'artists_count')
+        power_scaler = PowerTransformer(method='box-cox').fit(data)
+        artists_count = power_scaler.transform(data)
+        if self.verbose: self.plot_and_save(artists_count, 'artists_count', period='after')
+
+    def scale_registration_year(self):
+        # data = self.users['registration_unix_time'].apply(lambda year : gmtime(year).tm_year).values.reshape(-1, 1)
+        data = self.users['registration_unix_time'].values.reshape(-1, 1)
+        if self.verbose:  self.plot_and_save(data, 'registration_unix_time', bins = 20)
+
+    def scale_last_activity_year(self):
+        indices_to_impute = self.users[self.users.last_activity_unix_time < 10000000].index.values
+        avg_activity = self.users.activity_period_unix_time.mean()
+        for i in indices_to_impute:
+            self.users.at[i, 'last_activity_unix_time'] = self.users.iloc[i]['registration_unix_time'] + avg_activity
+
+        # data = self.users['last_activity_unix_time'].apply(lambda year : gmtime(year).tm_year).values.reshape(-1, 1)
+        data = self.users['last_activity_unix_time'].values.reshape(-1, 1)
+        if self.verbose:  self.plot_and_save(data, 'last_activity_unix_time', bins = 20)
+
+    def scale_activity_period_days(self):
+        self.users['activity_period_days'] = ((self.users['last_activity_unix_time'] - self.users['registration_unix_time'])/(60*60*24)).astype(int)
+        data = self.users['activity_period_days']
+        if self.verbose:  self.plot_and_save(data, 'activity_period_days')
 
     def scale_tracks(self):
         self.scale_track_duration()
@@ -49,7 +92,29 @@ class Dataset:
             plt.show()
         self.scalers['tracks'][feature] = power_scaler
         self.tracks[feature] = data
+        return
 
+    def tracks_split_zeros(self, feature, discrete_to_bins=True, bins=4):
+        zeros = self.tracks[feature] == 0
+        self.tracks['{}_zeros'.format(feature)] = zeros
+
+        if not discrete_to_bins:
+            return
+
+        not_zeros = self.tracks[feature][zeros == False].values.reshape(-1, 1)
+        est = KBinsDiscretizer(n_bins=bins, encode='ordinal', strategy='quantile')
+        est = est.fit(not_zeros)
+
+        for bin in range(bins):
+            bigger_than = self.tracks[feature] >= est.bin_edges_[0][bin]
+
+            if bin != bins - 1:
+                small_than = self.tracks[feature] < est.bin_edges_[0][bin + 1]
+            else:
+                small_than = self.tracks[feature] <= est.bin_edges_[0][bin + 1]
+
+            this_bin = bigger_than & small_than
+            self.tracks['{}bin{}'.format(feature, bin)] = this_bin
 
     def scale_track_duration(self):
         # consider split to zeros and the rest
@@ -69,6 +134,8 @@ class Dataset:
 
     def scale_track_album_listeners(self):
         # consider split to zeros and the rest
+        missing = (self.tracks['album_listeners'] == -1).astype(int)
+        self.tracks['is_single'] = missing
         self.perform_log_scale('album_listeners')
 
     def scale_track_album_playcount(self):
@@ -94,8 +161,7 @@ class Dataset:
 
 if __name__ == '__main__':
     data = Dataset()
-    data.show_tracks_distributions()
-    data.scale_tracks()
+    data.scale_users(True)
 
 
 
